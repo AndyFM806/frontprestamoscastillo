e0cefcd84964dab736984df0fbece67cf4eff02a
ronaldordz7 <ronaldorodriguezacevedo7@gmail.com>
2025-05-09 02:01:28 -0500

    afs

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -16,7 +16,7 @@ document.getElementById("login-form").addEventListener("submit", async function 
     if (response.ok) {
       const data = await response.json();
 
-      localStorage.setItem("token", data.token);
+      sessionStorage.setItem("token", data.token);
       sessionStorage.setItem("usuario", usuario);
       window.location.href = "inicio.html";
     } else {
